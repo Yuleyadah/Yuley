48e106832bece070429771aee108e9ee67332167
Yuleyadah <edoreadah@gmail.com>
2026-02-11 05:43:39 -0800

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,18 @@
+#Rectangle measurement
+# Given values
+length=12
+Width=8
+
+ # calculate area
+Area=length * Width
+
+ # Calculate Perimeter
+perimeter= 2* length + Width
+
+
+print("Length:", length)
+print("Width:", Width)
+print("Area:", Area)
+print("Perimeter:", perimeter)
+
+#
